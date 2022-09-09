2a6ca9c4ad420ea58918beb24def294827d71a1e
Erik <eriksn07@gmail.com>
2022-09-08 23:49:33 -0300

    feat: Dashboard API

--- src/scripts/models/axios.js
+++ src/scripts/models/axios.js
@@ -1,3 +1,4 @@
+const token = localStorage.getItem('token')
 
 export const homepageList = axios.create({
     baseURL: 'http://localhost:6278',
@@ -11,5 +12,42 @@ export const loginAndRegister = axios.create({
     baseURL: 'http://localhost:6278',
     headers: {
         'Content-Type': 'application/json'
+    }
+})
+
+
+export const dashboard = axios.create({
+    baseURL: 'http://localhost:6278',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+    }
+
+})
+
+export const profile = axios.create({
+    baseURL: 'http://localhost:6278',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+    }
+
+})
+
+
+export const sector = axios.create({
+    baseURL: 'http://localhost:6278',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+    }
+})
+
+
+export const department = axios.create({
+    baseURL: 'http://localhost:6278',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
     }
 })
